36e506f2957696038ba7fa602e85db00ec8144b3
BIT <BIT@DESKTOP-ET39DN4>
2019-08-09 19:29:21 +0900

    문제 완료!

--- src/prob06/CalcApp.java
+++ src/prob06/CalcApp.java
@@ -3,7 +3,9 @@ package prob06;
 import java.util.Scanner;
 
 public class CalcApp {
-
+	private int a;
+	private int b;
+	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
@@ -12,17 +14,57 @@ public class CalcApp {
 			System.out.print( ">> " );
 			String expression = scanner.nextLine();
 			
+			String[] tokens = expression.split(" ");
 			if( "quit".equals( expression ) ) {
 				break;
 			}
 			
-			String[] tokens = expression.split( " " );
+//			String[] tokens = expression.split( " " );
 			
 			if( tokens.length != 3 ) {
 				System.out.println( ">> 알 수 없는 식입니다.");
 				continue;
 			}
+			int lvalue = Integer.parseInt(tokens[0]);
+			int rvalue = Integer.parseInt(tokens[2]);
 			
+			switch(tokens[1]) {
+			case"+":{
+				Add add = new Add();
+				add.setValue(lvalue,rvalue);
+				int result = add.calculate();
+				System.out.println(">>"+result);
+				break;
+			}
+			case"-":{
+				Sub sub = new Sub();
+				sub.setValue(lvalue,rvalue);
+				int result = sub.calculate();
+				System.out.println(">>"+result);
+				break;
+				
+			}
+			case"*":{
+				Mul mul = new Mul();
+				mul.setValue(lvalue,rvalue);
+				int result = mul.calculate();
+				System.out.println(">>"+result);
+				break;
+				
+			}
+			case"/":{
+				Div div = new Div();
+				div.setValue(lvalue,rvalue);
+				int result = div.calculate();
+				System.out.println(">>"+result);
+				break;
+				
+			}
+			default : {
+				System.out.println(">>지원하지 않는 연산입니다.");
+				break;
+			}
+			}
 
 			
 			/*  코드를 완성 합니다 */
